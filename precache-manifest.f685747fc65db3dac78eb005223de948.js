d1c25d7fe8389804a871b7c3fe9ae38c9cf15300
Ramunas <rambek@gmail.com>
2020-09-20 14:06:40 +0300

    Updates

---
+++ precache-manifest.f685747fc65db3dac78eb005223de948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cecbe5aaf96606dc0b6d60c0016e08",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1cb2984c6652f22074d0",
+    "url": "/tic-tac-toe/static/css/main.5c259561.chunk.css"
+  },
+  {
+    "revision": "d4beeb07b80aeb6b3f4c",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb2984c6652f22074d0",
+    "url": "/tic-tac-toe/static/js/main.d65238db.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
